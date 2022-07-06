f60f4f26ac9f9d5a97c648850f2527e58a5e0513
John Paxton <johnpaxton@gmail.com>
2022-07-06 15:24:09 -0400

    text/plain output

--- workspace/demos/src/main/java/microservices/demos/HelloWorldController.java
+++ workspace/demos/src/main/java/microservices/demos/HelloWorldController.java
@@ -1,13 +1,25 @@
 package microservices.demos;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	public static final String STATUS_MESSAGE = "Hello, World!";
 
+	@RequestMapping(produces = "text/plain", 
+			method = RequestMethod.GET, 
+			path = "/hello-world/as-text")
+	public String sayHelloAsText() {
+		return "[text/plain] " + STATUS_MESSAGE;
+	}
+	
 	@GetMapping("/hello-world")
 	public String sayHello() {
-		return "Hello, world!";
+		return STATUS_MESSAGE;
 	}
+
 }

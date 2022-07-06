053acda4c763bbcfbc99fdf64e6b73a774e9191c
John Paxton <johnpaxton@gmail.com>
2022-07-06 12:41:06 -0400

    Initial add of demo

---
+++ workspace/demos/src/main/java/microservices/demos/HelloWorldController.java
@@ -0,0 +1,13 @@
+package microservices.demos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello-world")
+	public String sayHello() {
+		return "Hello, world!";
+	}
+}

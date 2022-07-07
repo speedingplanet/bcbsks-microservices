496a08df84e9acc3b456ba92edd4259691ff282f
John Paxton <johnpaxton@gmail.com>
2022-07-07 16:26:10 -0400

    Solutions through part 5

--- workspace/solutions/src/main/java/microservices/solutions/MemberController.java
+++ workspace/solutions/src/main/java/microservices/solutions/MemberController.java
@@ -1,10 +1,24 @@
 package microservices.solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+	
+	private List<Member> members;
+	
+	public MemberController() {
+		members = new ArrayList<>();
+		members.add(new Member(1, "John", "Paxton", true));
+		members.add(new Member(2, "Amir", "Shami", true));
+		members.add(new Member(3, "Clint", "Brubakken", true));
+		members.add(new Member(4, "Joshua", "Wurtz", false));
+		members.add(new Member(5, "Kelsi", "Henderson", false));
+	}
 
 	@GetMapping("/members/status")
 	public String getMembersStatus() {
@@ -12,7 +26,7 @@ public class MemberController {
 	}
 	
 	@GetMapping("/members")
-	public Member getMember() {
-		return new Member(1, "John", "Paxton", true);
+	public List<Member> getMember() {
+		return members;
 	}
 }
--- workspace/solutions/src/test/java/microservices/solutions/MemberControllerTest.java
+++ workspace/solutions/src/test/java/microservices/solutions/MemberControllerTest.java
@@ -3,6 +3,8 @@ package microservices.solutions;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +16,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 class MemberControllerTest {
 	@LocalServerPort
 	private int port;
-	
+
 	@Autowired
 	TestRestTemplate client;
 
@@ -25,15 +27,39 @@ class MemberControllerTest {
 //		assertEquals("All members available", response);
 		assertThat(response).contains("members");
 	}
-	
+
 	@Test
 	void testGetMembers() throws Exception {
 		String testUrl = String.format("http://localhost:%d/members", port);
-		Member testMember = client.getForObject(testUrl, Member.class);
-		assertThat(testMember.getFirstName()).isEqualTo("John");
-		assertEquals(testMember, new Member(1, "John", "Paxton", true));
-		assertThat(testMember).isEqualTo(new Member(1, "John", "Paxton", true));
+		Member[] testMembers = client.getForObject(testUrl, Member[].class);
+		assertThat(testMembers[0].getFirstName()).isEqualTo("John");
+		assertEquals(testMembers[0], new Member(1, "John", "Paxton", true));
+		assertThat(testMembers[0]).isEqualTo(new Member(1, "John", "Paxton", true));
+	}
+
+	@Test
+	void testHowManyPolicyHolders() throws Exception {
+		String testUrl = String.format("http://localhost:%d/members", port);
+		Member[] testMembers = client.getForObject(testUrl, Member[].class);
+		int policyHolderCount = 0;
+		for (Member m : testMembers) {
+			if (m.isPolicyHolder())
+				policyHolderCount++;
+		}
+		assertThat(policyHolderCount).isGreaterThan(2);
+	}
+
+	@Test
+	void testAltHowManyPolicyHolders() throws Exception {
+		String testUrl = String.format("http://localhost:%d/members", port);
+		Member[] testMembers = client.getForObject(testUrl, Member[].class);
+		long policyHolderCount = Arrays.stream(testMembers)
+//				.filter(m -> m.isPolicyHolder())
+				.filter(Member::isPolicyHolder)
+				.count();
+		assertThat(policyHolderCount >= 2);
 		
+
 	}
 
 }

a2dcc44172bc8e8820a36612a4ac607d222af526
John Paxton <johnpaxton@gmail.com>
2022-07-08 11:53:01 -0400

    Part 6 solution

--- workspace/solutions/src/main/java/microservices/solutions/MemberController.java
+++ workspace/solutions/src/main/java/microservices/solutions/MemberController.java
@@ -2,15 +2,19 @@ package microservices.solutions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-	
+
 	private List<Member> members;
-	
+
 	public MemberController() {
 		members = new ArrayList<>();
 		members.add(new Member(1, "John", "Paxton", true));
@@ -24,9 +28,53 @@ public class MemberController {
 	public String getMembersStatus() {
 		return "All members available";
 	}
-	
+
 	@GetMapping("/members")
-	public List<Member> getMember() {
+	public List<Member> getMembers(@RequestParam Map<String, String> params) {
+		/*
+		 * filter on firstName filter on firstName and lastName filter on firstName and
+		 * lastName and policyHolder
+		 * 
+		 * And any combinations of the above
+		 */
+
+		Predicate<Member> filterMembers = member -> true;
+		if (params.containsKey("firstName")) {
+			filterMembers = filterMembers.and(member -> {
+				String paramFirstName = params.get("firstName");
+				String memberFirstName = member.getFirstName();
+				return memberFirstName.equals(paramFirstName);
+			});
+		}
+
+		if (params.containsKey("lastName")) {
+			filterMembers = filterMembers.and(member -> member.getLastName().equals(params.get("lastName")));
+		}
+		
+		if (params.containsKey("policyHolder")) {
+			boolean paramPolicyHolder = Boolean.parseBoolean(params.get("policyHolder"));
+			filterMembers = filterMembers.and(member -> member.isPolicyHolder() == paramPolicyHolder);
+		}
+
+		return members.stream().filter(filterMembers).toList();
+	}
+
+	@GetMapping("/members/params-as-args")
+	public List<Member> getMembersParamsAsArgs(@RequestParam(name = "firstName", required = false) String firstName,
+			@RequestParam(name = "lastName", required = false) String lastName,
+			@RequestParam(name = "policyHolder", required = false) boolean policyHolder
+
+	) {
 		return members;
+	}
+
+	@GetMapping("/members/simple-search")
+	public List<Member> getMembersSimpleSearch(
+			@RequestParam(name = "lastName", required = false) String searchLastName) {
+		if (searchLastName == null) {
+			return members;
+		}
+		return members.stream().filter(member -> member.getLastName().equals(searchLastName))
+				.collect(Collectors.toList());
 	}
 }

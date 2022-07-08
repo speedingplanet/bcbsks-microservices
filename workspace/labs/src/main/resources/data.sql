72b5b24dafd925f1a5764d9ebca4aaf5affda67e
John Paxton <johnpaxton@gmail.com>
2022-07-08 12:23:51 -0400

    Data for Members

---
+++ workspace/labs/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO members (id, first_Name, last_Name, policy_Holder)
+VALUES (1, 'John', 'Cleese', true);
+
+INSERT INTO members (id, first_Name, last_Name, policy_Holder)
+VALUES (2, 'Eric', 'Idle', true);
+
+INSERT INTO members (id, first_Name, last_Name, policy_Holder)
+VALUES (3, 'Terry', 'Gilliam', false);
+
+INSERT INTO members (id, first_Name, last_Name, policy_Holder)
+VALUES (4, 'Terry', 'Jones', false);
